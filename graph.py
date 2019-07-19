ea961cda4688861d79a949e5210bf4e0ac27e44a
Alexander Shupov <alexandr.sh.0107@mail.ru>
2019-07-19 14:38:19 +0300

    Added docstrings

--- graph.py
+++ graph.py
@@ -15,14 +15,13 @@ server = flask.Flask(__name__)
 app = dash.Dash(__name__, server=server, external_stylesheets=external_stylesheets)
 
 r = Rediska()
-params = r.get_all_params()
-objects = r.count_of_objects()
+params = r.get_all_params()  # все доступные параметры для отображения
+objects = r.count_of_objects()  # количество объектов
 
 # Создание layout
 app.layout = html.Div(children=[
     html.Div(children=[
             html.Div(id='hoverData'),
-
             dcc.Graph(id='graph'),
             html.Hr(),
             html.H3('Параметр:', id='aaa'),
@@ -47,6 +46,11 @@ app.layout = html.Div(children=[
 @app.callback(Output('object-selector', 'style'),
               [Input('ob-num', 'value')])
 def update_objects_selector(v):
+    """
+    Отображает/скрывает slider для выбора объекта в зависимости от value checkbox'а
+    :param v: выбран ли флажок checkbox
+    :return: style of slider
+    """
     if v:
         return {}
     else:
@@ -58,17 +62,24 @@ def update_objects_selector(v):
     [Input('selector', 'value'),
      Input('object-selector', 'value'),
      Input('ob-num', 'value')])
-def update_output(value, obj, i):
+def update_output(values, obj, i):
+    """
+    Обновляет график
+    :param values: активные параметры
+    :param obj: выбранный объект
+    :param i: контролировать ли объект
+    :return: graph-figure
+    """
     if not i:
         obj = -1
-    if not isinstance(value, list):
-        value = [value]
+    if not isinstance(values, list):
+        values = [values]
 
     fig = tls.make_subplots(rows=1, cols=1, shared_yaxes=True, shared_xaxes=True, vertical_spacing=0.009,
                             horizontal_spacing=0.009)
     fig['layout']['margin'] = {'l': 30, 'r': 10, 'b': 50, 't': 25}
     time = r.get_data_for_object(obj, 'Time')
-    for v in value:
+    for v in values:
         points = r.get_data_for_object(obj, v)
         fig.append_trace({'x': time, 'y': points, 'hoverinfo': "none", 'type': 'scatter', 'name': v}, 1, 1)
 
@@ -79,23 +90,31 @@ def update_output(value, obj, i):
 @app.callback(Output('hoverData', 'children'),
               [Input('graph', 'hoverData'),
                Input('selector', 'value')])
-def c(x, value):
-    if x:
-        if not isinstance(value, list):
-            value = [value]
-        leigh = len(x['points'])
+def update_hover_data(hover_data, values):
+    """
+    Обновляет hover данные в виде таблицы
+    :param hover_data: данные hover
+    :param values: активные параметры
+    :return: таблица hover данных
+    """
+    if hover_data:
+        if not isinstance(values, list):
+            values = [values]
+        leigh = len(hover_data['points'])
         data_string = ' '
         for i in range(0, leigh):
-            data_string = data_string + str(x['points'][i]['y']) + "|"
+            data_string = data_string + str(hover_data['points'][i]['y']) + "|"
         data = data_string.split('|')
-        data = [{'0': value[v], '1': data[v]} for v in range(0, len(value))]
+        data = [{'0': values[v], '1': data[v]} for v in range(0, len(values))]
         card = dbc.Card([
             dbc.CardBody([
                 dash_table.DataTable(
                     id='table',
                     columns=[
-                             {"name": ['Момент времени: {}'.format(str(x['points'][0]['x'])), 'Параметр'], "id": '0'},
-                             {"name": ['Момент времени: {}'.format(str(x['points'][0]['x'])), 'Значение'], "id": '1'}],
+                             {"name": ['Момент времени: {}'.format(str(hover_data['points'][0]['x'])),
+                                       'Параметр'], "id": '0'},
+                             {"name": ['Момент времени: {}'.format(str(hover_data['points'][0]['x'])),
+                                       'Значение'], "id": '1'}],
                     data=data,
                     style_cell={'textAlign': 'left'},
                     style_header={
